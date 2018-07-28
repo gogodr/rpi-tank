292edb6a5c697e740b5d4297e0354fec55b37062
gogodr <gogodr@hotmail.com>
2018-07-27 19:55:46 -0500

    first test

---
+++ index.js
@@ -0,0 +1,80 @@
+const Gpio = require('onoff').Gpio;
+const CronJob = require('cron').CronJob;
+
+let dispenseScheduledJob;
+let flotMeterSensor;
+let sensorTickCount = 0;
+
+async function setup(){    
+    flotMeterSensor = new Gpio(25, 'in', 'falling');
+    dispenseScheduledJob = new CronJob({
+        //cronTime: '00 00 01 * * *',
+        cronTime: '00 * * * * *',
+        onTick: async () => {
+            console.log('Start Dispense');
+            try {
+                await dispense();
+                console.log('SUCCESS')
+            } catch (e) {
+                if(e === 'TICKS_END') {
+                    
+                }else{
+                    console.log('Error dispensing', e);
+                }
+            }
+        },
+        start: true,
+        timeZone: 'America/Lima'
+    });
+
+}
+
+async function dispense() {
+    return new Promise((res, rej) => {
+        let dispensedAmount = 0;
+        let dispenseTicks = 0;
+        let lastTime = Date.now();
+        flotMeterSensor.watch(watchSensor);
+        const dispenseTask = new CronJob({
+            cronTime: '* * * * * *',
+            onTick: () => {
+                const currentTime = Date.now();
+                const deltaTime = currentTime - lastTime;
+                const mockReadValue = Math.random() + 5;
+                dispensedAmount += Math.round(mockReadValue * (deltaTime / 1000) * calibrationFactor);
+                dispenseTicks++;
+                console.log('Dispensed: ', dispensedAmount);
+                console.log('Delta Time: ', deltaTime);
+                consoe.log('Sensor Ticks: ', sensorTickCount);
+
+                if (dispensedAmount >= 600) {
+                    // Successful dispense
+                    dispenseTask.stop();
+                    flotMeterSensor.unwatch(watchSensor);
+                    return res()
+                }
+                if (dispenseTicks >= 30) {
+                    // Unsucessful dispense, check valve
+                    dispenseTask.stop();
+                    flotMeterSensor.unwatch(watchSensor);
+                    return rej('TICKS_END')
+                }
+                lastTime = currentTime;
+                sensorTickCount = 0;
+            },
+            start: true
+        });
+
+    });
+}
+function watchSensor(err, val){
+    if(err){
+        console.log('ERROR WITH READING')
+        console.log(err);
+    }
+    if(val == 0){
+        sensorTickCount++
+    }
+}
+
+setup();
